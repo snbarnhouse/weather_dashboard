52acba8cbac0aee76ad2d94848e0bc883d67ceb2
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-03-12 10:28:16 -0500

    Added current temp, uvindex, and other weather searches.

---
+++ script.js
@@ -0,0 +1,13 @@
+//API key
+var APIKey = "1743d71cea491649f0bd96f06af46d71"
+//To store search cities
+var city= "";
+//Variables
+var searchCity = $("#search-city");
+var currentCity = $("#current-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentTemp = $("#temperature");
+var currentUvindex = $("#uv-index");
+var currentWindSpeed = $("#wind-speed");
+var currentHumidty = $("#humidity");
